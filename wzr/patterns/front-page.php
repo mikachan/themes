90c71c6db9418d736504dab29012684011c53987
Sarah Norris <sarah@sekai.co.uk>
2023-07-16 20:38:51 +0100

    Move duotone filter to theme.json

--- wzr/patterns/front-page.php
+++ wzr/patterns/front-page.php
@@ -6,7 +6,7 @@
  * Inserter: no
  */
 ?>
-<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/highway.jpg","id":2456,"dimRatio":80,"overlayColor":"base","minHeight":100,"minHeightUnit":"vh","contentPosition":"top right","style":{"color":{"duotone":"var:preset|duotone|midnight"}},"textColor":"primary","layout":{"type":"constrained"}} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/highway.jpg","id":2456,"dimRatio":80,"overlayColor":"base","minHeight":100,"minHeightUnit":"vh","contentPosition":"top right","textColor":"primary","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover has-custom-content-position is-position-top-right has-primary-color has-text-color" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2456" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/highway.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group"><!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"textTransform":"lowercase"}},"textColor":"primary","fontSize":"xx-large"} /-->
 
