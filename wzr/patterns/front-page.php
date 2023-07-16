73e2c63a651e252d8163c0c19c0b6641ae503b43
Sarah Norris <sarah@sekai.co.uk>
2023-07-16 15:48:50 +0100

    Make site title larger, adjust spacing

--- wzr/patterns/front-page.php
+++ wzr/patterns/front-page.php
@@ -8,8 +8,10 @@
 ?>
 <!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/highway.jpg","id":2456,"dimRatio":80,"overlayColor":"base","minHeight":100,"minHeightUnit":"vh","contentPosition":"top right","style":{"color":{"duotone":"var:preset|duotone|midnight"}},"textColor":"primary","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover has-custom-content-position is-position-top-right has-primary-color has-text-color" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2456" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/highway.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group"><!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"textTransform":"lowercase"}},"textColor":"primary","fontSize":"large"} /-->
+<div class="wp-block-group"><!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"textTransform":"lowercase"}},"textColor":"primary","fontSize":"xx-large"} /-->
 
-<!-- wp:navigation {"textColor":"primary","overlayMenu":"always","icon":"menu","overlayBackgroundColor":"base","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"var:preset|spacing|20"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;padding-top:var(--wp--preset--spacing--20)"><!-- wp:navigation {"textColor":"primary","overlayMenu":"always","overlayBackgroundColor":"base","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
